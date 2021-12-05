654884ee2d5f07d9b1c6d136bd453b99a13dcc58
yoonhero <yoonhero06@naver.com>
2021-12-06 08:27:58 +0900

    feat: Solo Christmas Ai

--- jocoding_crawling.py
+++ jocoding_crawling.py
@@ -2,15 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+import requests
+import os
+
+category = "커플"
 
 driver = webdriver.Chrome(
     '/Users/yoonseonghyeon/Desktop/YSH/programming/solchristmas_ai/chromedriver')
 driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
 elem = driver.find_element_by_name("q")
-elem.send_keys("조코딩")
+elem.send_keys(category)
 elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 1
+
+SCROLL_PAUSE_TIME = 0.5
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
@@ -29,17 +34,29 @@ while True:
 
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 count = 1
+
 for image in images:
     try:
         image.click()
         time.sleep(2)
+
+        folerName = './data'
+        file_name = str(count) + ".jpg"
+        completeFileName = os.path.join(folerName, file_name)
+        print(completeFileName)
+
         imgUrl = driver.find_element_by_xpath(
-            '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
-        opener = urllib.request.build_opener()
-        opener.addheaders = [
-            ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-        urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+            '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
+
+        f = open(completeFileName, 'wb')
+        f.write(requests.get(imgUrl).content)
+        f.close()
+
+        # opener = urllib.request.build_opener(
+        # opener.addheaders = [
+        #     ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        # urllib.request.install_opener(opener)
+        # urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
         count = count + 1
     except:
         pass
---
+++ my_crawling.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+import requests
+import os
+
+
+class Crawling:
+    def __init__(this, category):
+        this.category = category
+        this.driver = webdriver.Chrome(
+            "/Users/yoonseonghyeon/Desktop/YSH/programming/solchristmas_ai/chromedriver")
+
+        this.SCROLL_PAUSE_TIME = 0.2
+        this.folderName = "./data"
+        this.IMAGE_PAUSE_TIME = 0.4
+        this.start()
+
+    def start(this):
+        this.driver.get(
+            "https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+        elem = this.driver.find_element_by_name('q')
+        elem.send_keys(this.category)
+        elem.send_keys(Keys.RETURN)
+
+        this.scrollToBottom()
+        this.imageCrawling()
+
+    def scrollToBottom(this):
+        this.last_height = this.driver.execute_script(
+            "return document.body.scrollHeight")
+
+        while True:
+            this.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+
+            time.sleep(this.SCROLL_PAUSE_TIME)
+
+            this.new_height = this.driver.execute_script(
+                "return document.body.scrollHeight")
+
+            if this.new_height == this.last_height:
+                try:
+                    this.driver.find_element_by_css_selector(".mye4qd").click()
+                except:
+                    break
+            this.last_height = this.new_height
+
+    def imageCrawling(this):
+        count = 97
+        this.images = this.driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+        for image in this.images:
+            try:
+                image.click()
+                time.sleep(this.IMAGE_PAUSE_TIME)
+
+                file_name = str(count) + ".jpg"
+
+                completeFileName = os.path.join(this.folderName, file_name)
+                imgUrl = this.driver.find_element_by_xpath(
+                    '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
+
+                f = open(completeFileName, "wb")
+                f.write(requests.get(imgUrl).content)
+                f.close()
+
+                count += 1
+
+            except:
+                pass
+
+        this.driver.close()
+
+
+crawler = Crawling("couple")
